d0643b450270b00886090f612a0741dd10469102
Odhran <mahonyodhran@gmail.com>
2022-10-11 18:59:46 +0100

    removed calculated_bmr table - now within the user

--- database/database.sql
+++ database/database.sql
@@ -2,8 +2,6 @@ CREATE DATABASE IF NOT EXISTS `bmr_calculator`;
 
 USE `bmr_calculator`;
 
-DROP TABLE IF EXISTS `calculated_bmr`;
-
 DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user`
@@ -13,21 +11,10 @@ CREATE TABLE `user`
      `weight` SMALLINT NOT NULL,
      `height` SMALLINT NOT NULL,
      `gender` CHAR(1) NOT NULL,
-     `email`  VARCHAR(60) NOT NULL,
+     `bmr` SMALLINT NOT NULL,
+     `email`  VARCHAR(60) DEFAULT NULL,
      PRIMARY KEY (`id`)
   )
 engine=innodb
 auto_increment=6
 DEFAULT charset=latin1;
-
-CREATE TABLE `calculated_bmr`
-  (
-     `id`      INT(11) NOT NULL auto_increment,
-     `bmr`     SMALLINT NOT NULL,
-     `user_id` INT(11) NOT NULL,
-     PRIMARY KEY (`id`),
-     CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES user(`id`)
-  )
-engine=innodb
-auto_increment=6
-DEFAULT charset=latin1; 
