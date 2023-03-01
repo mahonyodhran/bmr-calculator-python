cbee92f0396b9df6460ae68131098201324511e9
Odhran <mahonyodhran@gmail.com>
2023-03-01 23:01:54 +0000

    wrote proper tests

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,14 +1,17 @@
 """ Module to test any helper methods [may be broken down further in time]
 """
 from helper import mifflin_st_jeor
+from models.user import User
 
 
-# def test_mifflin_st_jeor_male():
-#     """Test a male calculation
-#     """
-#     assert mifflin_st_jeor(28, 100, 180, 'm') == 1990
+def test_mifflin_st_jeor_male():
+    """Test a male calculation
+    """
+    male = User(28, 100, 180, 'm')
+    assert mifflin_st_jeor(male) == 1990
     
-#     """Test a female calculation with same values except gender
-#     """
-# def test_mifflin_st_jeor_female():
-#     assert mifflin_st_jeor(28, 100, 180, 'f') == 1824
+    """Test a female calculation with same values except gender
+    """
+def test_mifflin_st_jeor_female():
+    female = User(28, 100, 180, 'f')
+    assert mifflin_st_jeor(female) == 1824
